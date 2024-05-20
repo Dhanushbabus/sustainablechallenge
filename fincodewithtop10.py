9a83db934038d4da0ffe2a4cdc39b4634ef17de7
Dhanushbabus <96130535+Dhanushbabus@users.noreply.github.com>
2024-05-21 00:15:27 +0530

    Update fincodewithtop10.py

--- fincodewithtop10.py
+++ fincodewithtop10.py
@@ -1,5 +1,11 @@
-import tempfile
+import os
 import streamlit as st
-temp_directory = tempfile.gettempdir()
-print("Temporary directory:", temp_directory)
-st.write (temp_directory)
+# Get the path of the user's directory
+user_dir = os.path.expanduser("~")
+
+# Assuming "box" is a subfolder within the user's directory
+box_folder = os.path.join(user_dir, "box")
+
+# Print the full path of the "box" folder
+print("Full path of the 'box' folder:", box_folder)
+st.write (box_folder)
